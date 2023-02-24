c2eab2a471758fe69b7a25ee0f9e2aa01109c216
Marcos Douglas <89559087+DOUGUEIRA@users.noreply.github.com>
2023-02-24 00:45:32 -0300

    Add files via upload

---
+++ Perímetro e área.py
@@ -0,0 +1,11 @@
+print("TESTE DGR")
+print("========================")
+lado = int(input("insira o lado: "))
+
+perimetro = lado*4
+
+print ("o perimetro do quadrado é:",perimetro)
+
+area = lado*lado
+
+print ( "A area do quadrado é:",area)
